aa8ede6ad371d0d169f661341a3668676babdf3e
Jeongmin Rhee <jerhee@student.42seoul.kr>
2022-08-17 19:56:18 +0900

    third commit

--- get_next_line.c
+++ get_next_line.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jerhee <jerhee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/12 12:26:08 by jerhee            #+#    #+#             */
-/*   Updated: 2022/08/16 20:45:45 by jerhee           ###   ########.fr       */
+/*   Created: 2022/08/17 10:39:28 by jerhee            #+#    #+#             */
+/*   Updated: 2022/08/17 19:13:30 by jerhee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,78 +16,97 @@
 #include <unistd.h>
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+char	*fill_res(char *temp)
 {
-	static char	*save;
-	char		*line;
-	char		*res;
-	char		*temp;
-	int			i;
-	int 		j;
-	int			byte;
+	char	*res;
+	int		i;
+	int		j;
 
-	line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	byte = read(fd, line, BUFFER_SIZE);
-	// printf("byte : %d\n", byte);
-	// printf("line : %s\n", line);
-	temp = NULL;
-	if (save)
-		temp = save;
-	// if (!byte)
-	// 	return (NULL);
-	while (byte)
-	{
-		// printf("b_temp : %s\n", temp);
-		temp = ft_strjoin(temp, line);
-		// printf("temp : %s\n", temp);
-		if (ft_strchr(line, '\n'))
-			break;
-		byte = read(fd, line, BUFFER_SIZE);
-		line[byte] = '\0';
-		// free(line);
-		// printf("line : %s\n", line);
-	}
-	// i = 0;
 	i = ft_strchr(temp, '\n');
 	if (i == 0)
 		i = ft_strlen(temp);
-	// printf("temp : %s\n", temp);
-	// printf("i : %d\n", i);
-	j = 0;
 	res = (char *)malloc(sizeof(char) * (i + 2));
-	while(j <= i)
+	j = 0;
+	while (j <= i)
 	{
 		res[j] = temp[j];
 		j++;
 	}
 	res[j] = '\0';
-	// printf("res0 : %s\n", res);
-	// printf("temp_len : %d\n", (int)ft_strlen(temp));
+	free(temp);
+	return (res);
+}
+
+char	*fill_save(char *temp)
+{
+	char	*save;
+	int		i;
+	int		j;
+
+	i = ft_strchr(temp, '\n');
+	if (i == 0)
+		i = ft_strlen(temp);
 	save = (char *)malloc(sizeof(char) * (ft_strlen(temp) - i));
 	j = 0;
-	// printf("res1 : %s\n", res);
-	while(temp[i + j + 1])
+	while (temp[i + j + 1])
 	{
 		save[j] = temp[i + j + 1];
-		// printf("res2 : %s\n", res);
 		j++;
 	}
 	save[j] = '\0';
 	if (j == 0)
 		save = NULL;
-	// printf("save : %s\n", save);
-	// printf("res3 : %s\n", res);
-	return (res);
+	free(temp);
+	return (save);
 }
 
-int main(void)
+char	*get_next_line(int fd)
 {
-	int fd;
+	static char	*save;
+	char		*line;
+	char		*temp;
+	int			byte;
 
-	fd = open("a.txt", O_RDONLY);
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	printf("%s", get_next_line(fd));
-	// printf("%s\n", get_next_line(fd));
+	if (fd < 0)
+		return (NULL);
+	line = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	temp = NULL;
+	if (save)
+		temp = save;
+	byte = 1;
+	while (byte)
+	{
+		byte = read(fd, line, BUFFER_SIZE);
+		if (byte == -1)
+		{
+			free(line);
+			return (NULL);
+		}
+		line[byte] = '\0';
+		temp = ft_strjoin(temp, line);
+		if (ft_strchr(line, '\n'))
+			break ;
+	}
+	free(line);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+	if (!ft_strlen(temp))
+	{
+		free(temp);
+		return (NULL);
+	}
+	save = fill_save(temp);
+	return (fill_res(temp));
 }
+
+// int main(void)
+// {
+// 	int fd;
+
+// 	fd = open("a.txt", O_RDONLY);
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	printf("%s", get_next_line(fd));
+// 	// printf("%s", get_next_line(fd));
+// 	// printf("%s\n", get_next_line(fd));
+// 	// while (1)
+// 	// {}
+// }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: jerhee <jerhee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 12:26:05 by jerhee            #+#    #+#             */
-/*   Updated: 2022/08/16 10:02:24 by jerhee           ###   ########.fr       */
+/*   Updated: 2022/08/17 18:55:48 by jerhee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,7 @@ char	*ft_strjoin(char *s1, char *s2)
 	{
 		res = (char *)malloc(sizeof(char) * (ft_strlen(s2) + 1));
 		ft_strlcpy(res, (char *)s2, ft_strlen(s2) + 1);
+		free(s1);
 		return (res);
 	}
 	if (!s1 || !s2)
@@ -81,27 +82,7 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (NULL);
 	ft_strlcpy(res, (char *)s1, s1_len + 1);
 	ft_strlcat(res, (char *)s2, s1_len + s2_len + 1);
-	//free(s1);
-	return (res);
-}
-
-char	*ft_strdup(char *s1)
-{
-	int		s1_len;
-	int		i;
-	char	*res;
-
-	s1_len = ft_strlen(s1);
-	res = (char *)malloc(sizeof(char) * (s1_len + 1));
-	if (!res)
-		return (0);
-	i = 0;
-	while (s1[i])
-	{
-		res[i] = s1[i];
-		i++;
-	}
-	res[i] = '\0';
+	free(s1);
 	return (res);
 }
 
@@ -113,7 +94,6 @@ int	ft_strchr(const char *s, int c)
 	while (1)
 	{
 		if (((unsigned char *)s)[i] == (unsigned char)c)
-			//return ((char *)s + i);
 			return(i);
 		if (((unsigned char *)s)[i] == '\0')
 			break ;
